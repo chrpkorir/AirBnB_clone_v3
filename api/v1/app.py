59a020c75d46297195552fc9d9a82d4f99e7645e
musaubrian <musaubrian15@gmail.com>
2022-10-22 21:27:29 +0300

    Chore(api): return status of the api

---
+++ api/v1/app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""returns the status of the API"""
+
+from os import getenv
+from models import storage
+from api.v1.views import app_views
+from flask import Flask
+from flask import Blueprint
+
+"""Host and port env variables"""
+host_env = getenv('HBNB_API_HOST') or '0.0.0.0'
+port_env = getenv('HBNB_API_PORT') or 5000
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def tear_down_db():
+    """closes the sdb session"""
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(
+            host=host_env, port=port_env,
+            debug=True, threaded=True
+            )
---
+++ api/v1/views/index.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+returns a json object; the status
+"""
+
+from flask import jsonify
+from api.v1.views import app_views
+
+@app_views.route('/status')
+def status():
+    """
+    returns the status of the request
+    """
+    return jsonify({"status": "OK"})
