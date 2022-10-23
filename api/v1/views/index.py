398c369f09a32adbab6d4eb5a6cb0b9b93646229
musaubrian <musaubrian15@gmail.com>
2022-10-23 20:35:57 +0300

    Chore: retrieves the number of each objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,7 @@ returns a json object; the status
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status')
@@ -12,4 +13,21 @@ def status():
     """
     returns the status of the request
     """
-    return jsonify({"status": "OK"})
+    api_status = {"status": "OK"}
+    return jsonify(api_status)
+
+@app_views.route('/stats')
+def stats():
+    """
+    retrieves the number of each objects by type
+    """
+    objects = {
+            "amenities": storage.count("Amenity"),
+            "cities": storage.count("City"),
+            "places": storage.count("Place"),
+            "reviews": storage.count("Review"),
+            "states": storage.count("State"),
+            "users": storage.count("User")
+            }
+
+    return jsonify(objects)
