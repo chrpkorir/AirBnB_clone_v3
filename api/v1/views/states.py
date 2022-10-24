3d7a509fed8d7f192c22724aae9346c2fcd03dbb
musaubrian <musaubrian15@gmail.com>
2022-10-24 14:12:46 +0300

    Chore: delete a state object

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -4,7 +4,7 @@ handles all default RESTFul API actions to states
 """
 
 from api.v1.views import app_views
-from flask import abort, jsonify
+from flask import abort, jsonify, make_response
 from models.state import State
 from models import storage
 
@@ -26,11 +26,32 @@ def all_states():
 def list_state(state_id):
     """
     returns a single state object
+
+    Args::
+        state_id: the state object's id
     """
-    state = storage.get('State', state_id)
-    if state is None:
+    get_state = storage.get('State', state_id)
+    if get_state is None:
         abort(404)
     else:
-        state_response = state.to_dict()
+        state_response = get_state.to_dict()
 
     return jsonify(state_response)
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'])
+def delete_state(state_id):
+    """
+    deletes a state object using its id
+
+    Args::
+        state_id: the id of the state object
+    """
+    get_state = storage.get('State', state_id)
+    if get_state is None:
+        abort(404)
+    else:
+        storage.delete(get_state)
+        storage.save()
+
+    return make_response(jsonify({}), 200)
