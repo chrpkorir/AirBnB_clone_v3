bf65d8eeb8a998364b19c73ee422163c5facc6b4
musaubrian <musaubrian15@gmail.com>
2022-10-25 16:42:23 +0300

    Chore: get single amenity object using id

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -2,7 +2,7 @@
 """ handles API calls to the amenity """
 
 from api.v1.views import app_views
-from flask import jsonify
+from flask import abort, jsonify
 from models.amenity import Amenity
 from models import storage
 
@@ -13,3 +13,12 @@ def amenity_objs():
     amenities_objs = storage.all(Amenity).values()
     amenities_response = [obj.to_dict() for obj in amenities_objs]
     return jsonify(amenities_response)
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['GET'])
+def amenity_by_id(amenity_id=None):
+    """Retrieves an Amenity object by its id"""
+    amenity_obj = storage.get(Amenity, amenity_id)
+    if amenity_obj:
+        return jsonify(amenity_obj.to_dict())
+    abort(404)
