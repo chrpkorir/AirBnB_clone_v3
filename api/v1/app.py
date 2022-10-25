dc969ad1adab35b0369933cdb5566d4a9c5cef86
musaubrian <musaubrian15@gmail.com>
2022-10-25 17:30:01 +0300

    Chore: create CORS instance

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,6 +5,7 @@ from os import getenv
 from models import storage
 from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
+from flask_cors import CORS
 
 """Host and port env variables"""
 host_env = getenv('HBNB_API_HOST') or '0.0.0.0'
@@ -14,6 +15,9 @@ port_env = getenv('HBNB_API_PORT') or 5000
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
+
+"""CORS instance allowing: /* for 0.0.0.0"""
+cors = CORS(app, resources={"/*": {"0.0.0.0"}})
 
 
 @app.teardown_appcontext
