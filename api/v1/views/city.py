41f8def2ddfc215ab841cfd40b26d2425bb3f3c5
musaubrian <musaubrian15@gmail.com>
2022-10-25 13:49:00 +0300

    Chore: add a new city object

--- api/v1/views/city.py
+++ api/v1/views/city.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""handle API functionson the city object"""
+"""handle API functions on the city object"""
 
 from api.v1.views import app_views
-from flask import Flask, abort, jsonify, make_response
+from flask import Flask, Request, request, abort, jsonify, make_response
 from models.city import City
 from models import storage
 
@@ -12,10 +12,10 @@ def all_cities(state_id):
     """
     returns all the city objects
     """
-    get_state = storage.get('State', state_id)
-    if get_state is None:
+    state = storage.get('State', state_id)
+    if state is None:
         abort(404)
-    cities_response = [city.to_dict() for city in get_state.cities]
+    cities_response = [city.to_dict() for city in state.cities]
 
     return jsonify(cities_response)
 
@@ -25,11 +25,11 @@ def list_cities(city_id):
     """
     returns a single city object
     """
-    get_cities = storage.get('City', city_id)
-    if get_cities is None:
+    cities = storage.get('City', city_id)
+    if cities is None:
         abort(404)
 
-    cities_response = get_cities.to_dict()
+    cities_response = cities.to_dict()
     return jsonify(cities_response)
 
 
@@ -46,3 +46,23 @@ def delete_city(city_id):
     storage.save()
 
     return make_response(jsonify({}), 200)
+
+
+@app_views.route('/cities/<city_id>', method=['POST'])
+def add_city(state_id):
+    """
+    post a city object
+    """
+    if not Request.json:
+        abort(400, "Not a JSON")
+    data = Request.json
+    if 'name' not in data.keys():
+        abort(400, "Missing name")
+
+    state = storage.get("State", state_id)
+    if state is None:
+            abort(404)
+    data['state_id'] = state_id
+    instance = City(**data)
+    storage.new(instance)
+    storage.save()
