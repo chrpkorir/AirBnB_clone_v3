dbcc30f4bca91f8253682241258f4e440bdb1e6c
musaubrian <musaubrian15@gmail.com>
2022-10-25 13:15:32 +0300

    Chore: return single city object

--- api/v1/views/city.py
+++ api/v1/views/city.py
@@ -18,3 +18,16 @@ def all_cities(state_id):
     cities_response = [city.to_dict() for city in get_state.cities]
 
     return jsonify(cities_response)
+
+
+@app_views.route('/cities/<city_id>')
+def list_cities(city_id):
+    """
+    returns a single city object
+    """
+    get_cities = storage.get('City', city_id)
+    if get_cities is None:
+        abort(404)
+
+    cities_response = get_cities.to_dict()
+    return jsonify(cities_response)
