150734b7e4e37a32f31027a0504b7425bc5ebc43
musaubrian <musaubrian15@gmail.com>
2022-10-25 16:55:28 +0300

    Chore: add a new amenity object

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -2,7 +2,7 @@
 """ handles API calls to the amenity """
 
 from api.v1.views import app_views
-from flask import abort, jsonify, make_response
+from flask import abort, jsonify, make_response, request
 from models.amenity import Amenity
 from models import storage
 
@@ -33,3 +33,18 @@ def delete_amenity(amenity_id=None):
         storage.save()
         return make_response(jsonify({}), 200)
     abort(404)
+
+
+@app_views.route('/amenities', methods=['POST'])
+def amenity_post(state_id=None):
+    """Create an Amenity object"""
+    if not request.get_json():
+        return make_response(jsonify({'error': 'Not a JSON'}), 400)
+    if 'name' not in request.get_json():
+        return make_response(jsonify({'error': 'Missing name'}), 400)
+
+    dict_body = request.get_json()
+    new_amenity = Amenity(**dict_body)
+    storage.new(new_amenity)
+    storage.save()
+    return make_response(jsonify(new_amenity.to_dict()), 201)
